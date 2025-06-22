3106a217e856f620e8a22353d2315eb0016fb92e
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2025-06-22 16:06:38 -0300

    Making LinkedList iterable

--- src/app/DataStructureSamples.hpp
+++ src/app/DataStructureSamples.hpp
@@ -21,17 +21,47 @@ class LinkedList;
 
 template <ColorableAndOrdered T>
 class LinkedList : public Visualizable {
-
   private:
     SingleLinkedNode<T> * head {nullptr};
     SingleLinkedNode<T> * last {head};
     int saved_elements {0};
 
   public:
+    class Iterator {
+      using Node = SingleLinkedNode<T>;
+      private:
+        Node * current;
+
+      public:
+
+        Iterator(Node * starting_point) : current(starting_point) {};
+
+        T& operator*() { return current->data; }
+
+        // const means: We won't modify the Iterator
+        bool operator!=(const Iterator& other) const {
+          return current != other.current;
+        }
+
+        Iterator& operator++() {
+          current = current->next;
+          return *this;
+        }
+
+    };
+
     bool add(T data);
     bool remove(T data);
     bool contains(T data);
     std::optional<T> get(int idx);
+
+    Iterator begin() {
+      return Iterator(head);
+    }
+
+    Iterator end() {
+      return Iterator(nullptr);
+    }
 
     // Removes and returns head
     inline std::optional<T> pull() {

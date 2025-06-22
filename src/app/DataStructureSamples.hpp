017944d78375db83495ebc0ad61b4d637a60b5d5
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2025-06-22 15:36:09 -0300

    Feat: Working on the visual generic LinkedList

--- src/app/DataStructureSamples.hpp
+++ src/app/DataStructureSamples.hpp
@@ -1,24 +1,64 @@
+#pragma once
+
 #include "../include/app/Visualizable.hpp"
+#include <concepts>
+#include <optional>
 #include <type_traits>
 
 // type_trait
 // similar to the extension of generic types in Java
 template<typename T>
-concept ColorableType = std::is_base_of<Colorable, T>::value;
+concept ColorableAndOrdered = std::is_base_of<Colorable, T>::value && std::totally_ordered<T>;
 
-template <typename T>
+template <ColorableAndOrdered T>
 struct SingleLinkedNode {
   T data;
   SingleLinkedNode * next;
 };
 
-template <typename T>
+template <ColorableAndOrdered T>
 class LinkedList;
 
-template <typename T>
+template <ColorableAndOrdered T>
 class LinkedList : public Visualizable {
-  public:
+
   private:
     SingleLinkedNode<T> * head {};
+    SingleLinkedNode<T> * last {head};
+    int saved_elements {0};
+
+  public:
+    bool add(T data);
+    bool remove(T data);
+    bool contains(T data);
+    std::optional<T> get(int idx);
+
+    // Removes and returns head
+    inline std::optional<T> pull() {
+      T return_value = getFirst();
+      removeFirst();
+      return return_value;
+    }
+
+    inline bool removeFirst() {
+      return remove(getFirst());
+    }
+
+    inline bool removeLast() {
+      return remove(getLast());
+    }
+
+    inline int size() {
+      return saved_elements;
+    }
+
+    inline T getFirst() {
+      return head->data;
+    }
+
+    inline T getLast() {
+      return last->data;
+    }
+
 };
 
